954b6918094a3da6000ee276c9d43b6b437cf4d2
Anil Kumar <gitpack@users.noreply.github.com>
2013-10-20 16:10:44 +0530

    Create FolderHandler.java

---
+++ FolderHandler.java
@@ -0,0 +1,31 @@
+package kick;
+
+import org.apache.hadoop.io.Text;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class FolderHandler extends DefaultHandler {
+	 Text value=null;
+	 public Text getValue() {
+		return value;
+	}
+	public void setValue(Text value) {
+		this.value = value;
+	}
+	public void startElement (String uri, String localName,String qName, Attributes attributes) throws SAXException
+	    {
+		 	System.out.println(attributes.getValue("Id") +" " + attributes.getValue("Reputation"));
+	    	//if(attributes.getValue("PostTypeId").equals("2")){
+	    		Text keyWords =new Text(attributes.getValue("Id"));
+				Text valueWords = new Text(attributes.getValue("Reputation"));
+				String val=keyWords.toString()+" "+valueWords.toString();
+				// Write the sentence 
+				//System.out.println(keyWords.toString() + " :: " + val);
+				if(keyWords != null && valueWords != null){
+					setValue(new Text(val));
+				}
+	    	//}
+	    }
+
+}
