aa40984e8286924a2e67ec9cb91342b2d1ce6453
Anil Kumar <gitpack@users.noreply.github.com>
2013-10-25 17:40:16 +0530

    Manual Parsing of xml file line by line
    I used string tokenizer and split in tandem to extract required pair of{OwnerUserId, ParentId} from posts.xml of StackOverflow`s data dump.

--- Map.java
+++ Map.java
@@ -1,39 +1,50 @@
-package kick;
-
-import java.io.File;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.xml.sax.SAXException;
 
 public class Map extends Mapper<LongWritable, Text, Text, Text> {
-	File users=new File("hdfs://­localhost:54310/input/cooking1/users.xml");
-	FolderHandler handler = new FolderHandler(); 
 
-	/**
-	 * Given an output filename, write a bunch of random records to it.
-	 * @throws SAXException 
-	 * @throws ParserConfigurationException 
-	 * @throws InterruptedException 
-	 */
 	@Override
 	public void map(LongWritable key, Text value, Context context) throws InterruptedException {
-		try{
-		SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
-		parser.parse(users,handler);
-		Text val=new Text("yo");
-		if(value != null){
-			context.write(val, handler.getValue());
+		StringTokenizer tok= new StringTokenizer(value.toString());
+
+		String pa=null,ow=null,pi=null;
+		while (tok.hasMoreTokens()) {
+			String[] arr;
+			String val = (String) tok.nextToken();
+			if(val.contains("PostTypeId")){
+				arr= val.split("[\"]");
+				pi=arr[arr.length-1];
+				if(pi.equals("2")){
+					continue;
+				}
+				else break;
+			}
+			if(val.contains("ParentId")){
+				arr= val.split("[\"]");
+				pa=arr[arr.length-1];
+			} 
+			else if(val.contains("OwnerUserId") ){
+				arr= val.split("[\"]");
+				ow=arr[arr.length-1];
+				try {
+					if(pa!=null && ow != null)
+						context.write(new Text(ow),new Text(pa));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
+
+
 	}
+
 }
+
+
