5995fb777d9a32f73c3ca2242ed8148770528f36
Anil Kumar <gitpack@users.noreply.github.com>
2013-10-25 17:50:09 +0530

    Main class for job configuration
    I used string tokenizer and split in tandem to extract required pair of{OwnerUserId, ParentId} from posts.xml of StackOverflow`s data dump.

--- Recommend.java
+++ Recommend.java
@@ -1,29 +1,6 @@
-package kick;
-
-/*import java.io.File;
-import java.io.IOException;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;*/
-/*
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;*/
-
-//import java.io.File;
-
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -40,21 +17,24 @@ public class Recommend {
 		job.setJarByClass(Recommend.class);
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
+		Path outPath = new Path(args[1]);
+		FileSystem dfs = FileSystem.get(outPath.toUri(), conf);
+		if (dfs.exists(outPath)) {
+			dfs.delete(outPath, true);
+		}
+		
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
 		// the keys are text (strings)
-		job.setOutputKeyClass(NullWritable.class);
-		job.setMapOutputKeyClass(LongWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(Text.class);
 		// the values are text (strings)
 		job.setOutputValueClass(Text.class);
 		//job.setNumReduceTasks(0);
 		job.setMapperClass(Map.class);
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
-		Path outPath = new Path(args[1]);
-		FileSystem dfs = FileSystem.get(outPath.toUri(), conf);
-		if (dfs.exists(outPath)) {
-			dfs.delete(outPath, true);
-		}
+		
+		
 	}
 }
